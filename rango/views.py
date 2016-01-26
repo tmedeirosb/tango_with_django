c16e42e6f8a8b0f2fdf581150469ad35ba2d2507
tmedeirosb <thiago.mdeiros@gmail.com>
2016-01-26 13:50:43 -0300

    integração template e models a partir da view

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category, Page
 
 # Create your views here.
 def index(request):
     # return HttpResponse("Olá, Rango!")
-    context_dict = {'boldmessage': 'Dun dun!'}
+    #context_dict = {'boldmessage': 'Dun dun!'}
+    #return render(request, 'rango/index.html', context_dict)
 
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict  = {'categories': category_list}
     return render(request, 'rango/index.html', context_dict)
 
 
@@ -14,3 +18,21 @@ def about(request):
     dados = {'qualquer': 'informação qualquer!'}
 
     return render(request, 'rango/about.html', dados)
+
+
+def category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
